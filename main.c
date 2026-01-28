fa76e12e6a154838f4e1f9ceba1f18eea857b998
ashudevcodes <ashishprasad949@gmail.com>
2026-01-28 23:50:09 +0530

    takimg input from user and shutdown the qemu or machine
    now we taking :)

--- main.c
+++ main.c
@@ -8,57 +8,35 @@
 
 EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
- InitializeLib(ImageHandle, SystemTable);
-  // SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Switching to Serial Mode...\r\n");
-
-
-
+  InitializeLib(ImageHandle, SystemTable);
   Print(L"Hola Ashish \r\n");
+
   EFI_TIME Time;
-  // Print(L"Did you want to quit N/y: ");
-  // EFI_TIME efi_time;
-  // EFI_STATUS status = SystemTable->RuntimeServices->GetTime(&Time,NULL);
   EFI_STATUS status = uefi_call_wrapper(RT->GetTime, 2, &Time, NULL);
 
-  
   if (EFI_ERROR(status)) {
 	Print(L"Fail to get time: %r\n", status);
 	return status;
   }
 
   Print(L"Current Second is: %02d\n",(UINT32)Time.Second);
+  Print(L"Did you want to quit N/y: ");
 
-  // SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &EventIndex);
-  // SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
-  // SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &EventIndex);
-  // SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
-
- //  if (key.UnicodeChar == L'y' || key.UnicodeChar == L'Y') {
-	// Print(L"Shutting down Bye Bye!\r\n");
-	// SystemTable->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
- //  } else {
-	// Print(L"Continue to UEFI Shell.. \r\n");
- //  }
   EFI_INPUT_KEY key;
 
-  for (; ;) {
+  WaitForSingleEvent(ST->ConIn->WaitForKey, 0);
+  EFI_STATUS Status = uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2,ST->ConIn, &key);
 
-	WaitForSingleEvent(ST->ConIn->WaitForKey, 0);
-	EFI_STATUS Status = uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2,ST->ConIn, &key);
+  if (EFI_ERROR(Status)) {
+	DEBUG((D_ERROR, "Input: error return from ReadKey %x\n", Status));
+	return Status;
+  }
 
-	if (EFI_ERROR(Status)) {
-	  DEBUG((D_ERROR, "Input: error return from ReadKey %x\n", Status));
-	  break;
-	}
-
-	if (key.UnicodeChar == L'y' || key.UnicodeChar == L'Y') {
-	  Print(L"Shutting down Bye Bye!\r\n");
-	  ST->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
-	  break;
-	} else {
-	  Print(L"Continue to UEFI Shell.. \r\n");
-	}
-
+  if (key.UnicodeChar == L'y' || key.UnicodeChar == L'Y') {
+	Print(L"Shutting down Bye Bye!\r\n");
+	uefi_call_wrapper(RT->ResetSystem, 4, EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+  } else {
+	Print(L"Continue to UEFI Shell.. \r\n");
   }
 
   return EFI_SUCCESS;
